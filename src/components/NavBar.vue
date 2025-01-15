c95b100fcbfe8cd92210b00f976bb881bd6117d0
ErikTitan <102426162+ErikTitan@users.noreply.github.com>
2025-01-15 03:58:46 +0100

    feat(NavBar): add Presets and Contact menu items - Add 'Presets' menu item with icon and route to '/presets' - Add 'Contact' menu item with icon and route to '/contact' - Remove 'Menu' item with nested options

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -27,14 +27,15 @@ export default {
                     route: '/equalizer'
                 },
                 {
-                    label: 'Menu',
-                    icon: 'pi pi-fw pi-bars',
-                    items: [
-                        { label: 'Option 1', icon: 'pi pi-fw pi-check' },
-                        { label: 'Option 2', icon: 'pi pi-fw pi-times' },
-                    ],
+                    label: 'Presets',
+                    icon: 'pi pi-wave-pulse',
+                    route: '/presets'
                 },
-                { label: 'Contact', icon: 'pi pi-fw pi-envelope' },
+                {
+                    label: 'Contact',
+                    icon: 'pi pi-fw pi-envelope',
+                    route: '/contact'
+                },
             ],
             isDarkMode: false,
             isScrolled: false
@@ -92,11 +93,6 @@ export default {
                 <a v-else v-ripple class="flex items-center" v-bind="props.action">
                     <i :class="item.icon" />
                     <span class="ml-2">{{ item.label }}</span>
-                    <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
-                    <span v-if="item.shortcut" class="ml-auto border rounded bg-emphasis text-muted-color text-xs p-1">
-                        {{ item.shortcut }}
-                    </span>
-                    <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto']"></i>
                 </a>
             </template>
             <template #end>
